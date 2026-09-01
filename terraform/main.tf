5a7ea7f70dd138edf89a946c58e8ea52f4b98ca4
ssp001 <shovansaha70@gmail.com>
2026-09-01 17:17:49 +0530

    added lambdas for services getway

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,24 +62,42 @@ resource "aws_api_gateway_authorizer" "this" {
 resource "aws_api_gateway_resource" "this_process" {
   rest_api_id = aws_api_gateway_rest_api.this.id
   parent_id   = aws_api_gateway_rest_api.this.root_resource_id
-  path_part   = "process"
+  path_part   = "Home/process"
 }
 resource "aws_api_gateway_resource" "this_delete" {
   rest_api_id = aws_api_gateway_rest_api.this.id
   parent_id   = aws_api_gateway_rest_api.this.root_resource_id
-  path_part   = "delete"
+  path_part   = "Home/delete"
 }
 
 resource "aws_api_gateway_resource" "this_respones" {
   rest_api_id = aws_api_gateway_rest_api.this.id
   parent_id   = aws_api_gateway_rest_api.this.root_resource_id
-  path_part   = "respones"
+  path_part   = "Home/respones"
 }
 
+resource "aws_api_gateway_resource" "health_chek" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
+  path_part   = "Home"
+}
 
 #############################
 // getway endpoints
 #############################
+
+resource "aws_api_gateway_method" "this_home" {
+  resource_id   = aws_api_gateway_rest_api.this.id
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  http_method   = "GET"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.this.id
+  depends_on = [
+    aws_api_gateway_authorizer.this
+  ]
+}
+
+
 
 resource "aws_api_gateway_method" "post_respones" {
   rest_api_id   = aws_api_gateway_rest_api.this.id
